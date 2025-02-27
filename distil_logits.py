5525e9ab46f23f157c81bcaaae08ca9d535c1a28
kaan <kaansirin@yahoo.com>
2025-02-27 14:39:04 +0100

    logit based distillation

--- distil_logits.py
+++ distil_logits.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import torch
 import torch.nn.functional as F
@@ -6,23 +7,41 @@ from trl import SFTTrainer, SFTConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 from accelerate import Accelerator
 import yaml
+from dotenv import load_dotenv
+from datetime import datetime
+
+log_dir = "./logs"
+os.makedirs(log_dir, exist_ok=True)
+log_filename = f"{log_dir}/distil_logits_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler(log_filename), logging.StreamHandler()],
+)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 # Configuration
 config = {
     "project_name": "distil-logits",
     "dataset": {
-        "name": "mlabonne/FineTome-100k",
+        "name": "qiaojin/PubMedQA",
         "split": "train",
-        # "num_samples": , # You can pass a number here to limit the number of samples to use.
-        "seed": 42
+        "num_samples": 3000,
+        "seed": 42,
+        "subset": "pqa_artificial",
     },
     "models": {
-        "teacher": "arcee-ai/Arcee-Spark",
-        "student": "Qwen/Qwen2-1.5B"
+        "teacher": "meta-llama/Llama-3.2-3B-Instruct",
+        "student": "meta-llama/Llama-3.2-1B-Instruct",
     },
     "tokenizer": {
-        "max_length": 4096,
-        "chat_template": "{% for message in messages %}{% if loop.first and messages[0]['role'] != 'system' %}{{ '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n' }}{% endif %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
+        "max_length": 512,  # TODO: Had to change from 4096 due to memory issues
+        # TODO: Is chat_template necessary for MedQA-SWE?
+        # "chat_template": "{% for message in messages %}{% if loop.first and messages[0]['role'] != 'system' %}{{ '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n' }}{% endif %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
     },
     "training": {
         "output_dir": "./results",
@@ -37,67 +56,81 @@ config = {
         "lr_scheduler_type": "cosine",
         "resume_from_checkpoint": None,  # Set to a path or True to resume from the latest checkpoint
         "fp16": False,
-        "bf16": True
+        "bf16": True,
     },
-    "distillation": {
-        "temperature": 2.0,
-        "alpha": 0.5
-    },
-    "model_config": {
-        "use_flash_attention": True
-    }
+    "distillation": {"temperature": 2.0, "alpha": 0.5},
+    "model_config": {"use_flash_attention": False},
     # "spectrum": {
     #     "layers_to_unfreeze": "/workspace/spectrum/snr_results_Qwen-Qwen2-1.5B_unfrozenparameters_50percent.yaml" # You can pass a spectrum yaml file here to freeze layers identified by spectrum.
     # }
 }
 
 # Set up environment
-os.environ['WANDB_PROJECT'] = config["project_name"]
+os.environ["WANDB_PROJECT"] = config["project_name"]
 accelerator = Accelerator()
 device = accelerator.device
 
 # Load and preprocess dataset
-dataset = load_dataset(config["dataset"]["name"], split=config["dataset"]["split"])
-dataset = dataset.shuffle(seed=config["dataset"]["seed"])
+dataset = (
+    load_dataset(
+        config["dataset"]["name"],
+        config["dataset"]["subset"],
+        split=config["dataset"]["split"],
+    )
+    if config["dataset"]["subset"]
+    else load_dataset(config["dataset"]["name"], split=config["dataset"]["split"])
+)
+
+# First take a subset of the dataset if specified, then shuffle
 if "num_samples" in config["dataset"]:
     dataset = dataset.select(range(config["dataset"]["num_samples"]))
+dataset = dataset.shuffle(seed=config["dataset"]["seed"])
 
 # Load tokenizers
-teacher_tokenizer = AutoTokenizer.from_pretrained(config["models"]["teacher"])
-student_tokenizer = AutoTokenizer.from_pretrained(config["models"]["student"])
+teacher_tokenizer = AutoTokenizer.from_pretrained(
+    config["models"]["teacher"], token=HF_TOKEN
+)
+student_tokenizer = AutoTokenizer.from_pretrained(
+    config["models"]["student"], token=HF_TOKEN
+)
+# TODO: Is this necessary?
+if student_tokenizer.pad_token is None:
+    student_tokenizer.pad_token = student_tokenizer.eos_token
 
 # Apply chat template to student tokenizer
-student_tokenizer.chat_template = config["tokenizer"]["chat_template"]
+# TODO: is chat_template necessary for MedQA-SWE?
+# student_tokenizer.chat_template = config["tokenizer"]["chat_template"]
 
-def sharegpt_format(example):
-    conversations = example['conversations']
-    message = []
-    
-    if isinstance(conversations, list):
-        for conversation in conversations:
-            if isinstance(conversation, dict):
-                if conversation.get('from') == 'human':
-                    message.append({"role": "user", "content": conversation.get('value', '')})
-                elif conversation.get('from') == 'gpt':
-                    message.append({"role": "assistant", "content": conversation.get('value', '')})
-                elif conversation.get('from') == 'system':
-                    message.insert(0, {"role": "system", "content": conversation.get('value', '')})
 
-    if not any(msg.get('role') == 'system' for msg in message):
-        message.insert(0, {"role": "system", "content": "You are a helpful assistant."})
+# Dataset specific formatting
+def pubmedqa_format(sample):
+    prompt = (
+        f"You are a biomedical expert. Provide a concise answer to the following biomedical question. \n\n"
+        f"Question: {sample['question']}\n\n"
+        f"Answer: {sample['long_answer']}"
+    )
 
-    text = student_tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=True)
-    return {"text": text}
+    return {"prompt": prompt}
+
 
 # Preprocess and tokenize the dataset
 print("Preprocessing and tokenizing dataset...")
 original_columns = dataset.column_names
-dataset = dataset.map(sharegpt_format, remove_columns=original_columns)
+dataset = dataset.map(pubmedqa_format, remove_columns=original_columns)
+
 
 def tokenize_function(examples):
-    return student_tokenizer(examples["text"], truncation=True, max_length=config["tokenizer"]["max_length"], padding="max_length")
+    return student_tokenizer(
+        examples["prompt"],
+        truncation=True,
+        max_length=config["tokenizer"]["max_length"],
+        padding="max_length",
+    )
 
-tokenized_dataset = dataset.map(tokenize_function, batched=True, num_proc=8, remove_columns=["text"])
+
+tokenized_dataset = dataset.map(
+    tokenize_function, batched=True, num_proc=8, remove_columns=["prompt"]
+)
 tokenized_dataset = tokenized_dataset.train_test_split(test_size=0.1)
 
 print("Dataset preparation complete. Loading models...")
@@ -107,15 +140,20 @@ model_kwargs = {"torch_dtype": torch.bfloat16}
 if config["model_config"]["use_flash_attention"]:
     model_kwargs["attn_implementation"] = "flash_attention_2"
 
-teacher_model = AutoModelForCausalLM.from_pretrained(config["models"]["teacher"], **model_kwargs)
-student_model = AutoModelForCausalLM.from_pretrained(config["models"]["student"], **model_kwargs)
+teacher_model = AutoModelForCausalLM.from_pretrained(
+    config["models"]["teacher"], **model_kwargs
+)
+student_model = AutoModelForCausalLM.from_pretrained(
+    config["models"]["student"], **model_kwargs
+)
 
 # Optionally freeze layers of the student model based on spectrum configuration
 if "spectrum" in config and "layers_to_unfreeze" in config["spectrum"]:
+
     def freeze_student_spectrum(model, unfrozen_layers_file):
-        with open(unfrozen_layers_file, 'r') as file:
-            unfrozen_layers = yaml.safe_load(file)['unfrozen_parameters']
-        
+        with open(unfrozen_layers_file, "r") as file:
+            unfrozen_layers = yaml.safe_load(file)["unfrozen_parameters"]
+
         for name, param in model.named_parameters():
             if not any(layer in name for layer in unfrozen_layers):
                 param.requires_grad = False
@@ -125,47 +163,87 @@ if "spectrum" in config and "layers_to_unfreeze" in config["spectrum"]:
     # Apply freezing to student model
     freeze_student_spectrum(student_model, config["spectrum"]["layers_to_unfreeze"])
 else:
-    print("Spectrum configuration not found. All layers of the student model will be trainable.")
+    print(
+        "Spectrum configuration not found. All layers of the student model will be trainable."
+    )
+
 
 def pad_logits(student_logits, teacher_logits):
     student_size, teacher_size = student_logits.size(-1), teacher_logits.size(-1)
     if student_size != teacher_size:
         pad_size = abs(student_size - teacher_size)
-        pad_tensor = torch.zeros((*teacher_logits.shape[:-1], pad_size), dtype=teacher_logits.dtype, device=teacher_logits.device)
-        return (torch.cat([student_logits, pad_tensor], dim=-1), teacher_logits) if student_size < teacher_size else (student_logits, torch.cat([teacher_logits, pad_tensor], dim=-1))
+        pad_tensor = torch.zeros(
+            (*teacher_logits.shape[:-1], pad_size),
+            dtype=teacher_logits.dtype,
+            device=teacher_logits.device,
+        )
+        return (
+            (torch.cat([student_logits, pad_tensor], dim=-1), teacher_logits)
+            if student_size < teacher_size
+            else (student_logits, torch.cat([teacher_logits, pad_tensor], dim=-1))
+        )
     return student_logits, teacher_logits
 
+
 class LogitsTrainer(SFTTrainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
-        inputs = {k: v.to(model.device) if hasattr(v, 'to') else v for k, v in inputs.items()}
-        self.teacher_model = self.teacher_model.to(model.device)
-        
-        student_model = model.module if hasattr(model, 'module') else model
-        teacher_model = self.teacher_model.module if hasattr(self.teacher_model, 'module') else self.teacher_model
+    def compute_loss(
+        self, model, inputs, return_outputs=False, num_items_in_batch=None
+    ):
+        device = next(model.parameters()).device
+
+        inputs = {k: v.to(device) if hasattr(v, "to") else v for k, v in inputs.items()}
+        self.teacher_model = self.teacher_model.to(device)
+
+        student_model = model.module if hasattr(model, "module") else model
+        teacher_model = (
+            self.teacher_model.module
+            if hasattr(self.teacher_model, "module")
+            else self.teacher_model
+        )
 
         student_outputs = student_model(**inputs)
         with torch.no_grad():
             teacher_outputs = teacher_model(**inputs)
 
-        custom_loss = self.distillation_loss(student_outputs.logits, teacher_outputs.logits, inputs, student_outputs.loss)
+        custom_loss = self.distillation_loss(
+            student_outputs.logits, teacher_outputs.logits, inputs, student_outputs.loss
+        )
         return (custom_loss, student_outputs) if return_outputs else custom_loss
 
     def distillation_loss(self, student_logits, teacher_logits, inputs, original_loss):
-        student_logits, teacher_logits = pad_logits(student_logits.to(self.model.device), teacher_logits.to(self.model.device))
-        
+        device = next(self.model.parameters()).device
+
+        student_logits, teacher_logits = pad_logits(
+            student_logits.to(device), teacher_logits.to(device)
+        )
+
         student_logits_scaled = student_logits / config["distillation"]["temperature"]
         teacher_logits_scaled = teacher_logits / config["distillation"]["temperature"]
 
-        loss_kd = F.kl_div(
-            F.log_softmax(student_logits_scaled, dim=-1),
-            F.softmax(teacher_logits_scaled, dim=-1),
-            reduction='batchmean'
-        ) * (config["distillation"]["temperature"] ** 2) / config["tokenizer"]["max_length"]
+        loss_kd = (
+            F.kl_div(
+                F.log_softmax(student_logits_scaled, dim=-1),
+                F.softmax(teacher_logits_scaled, dim=-1),
+                reduction="batchmean",
+            )
+            * (config["distillation"]["temperature"] ** 2)
+            / config["tokenizer"]["max_length"]
+        )
 
-        return config["distillation"]["alpha"] * loss_kd + (1 - config["distillation"]["alpha"]) * original_loss
+        return (
+            config["distillation"]["alpha"] * loss_kd
+            + (1 - config["distillation"]["alpha"]) * original_loss
+        )
+
 
 # Training arguments
-training_arguments = TrainingArguments(**config["training"])
+# TODO: replacting training arg with SFTConfig
+# training_arguments = TrainingArguments(**config["training"])
+training_arguments = SFTConfig(
+    **config["training"],
+    max_seq_length=config["tokenizer"]["max_length"],
+    dataset_text_field="prompt",
+)
 
 # Create the custom SFT Trainer
 trainer = LogitsTrainer(
@@ -174,8 +252,6 @@ trainer = LogitsTrainer(
     eval_dataset=tokenized_dataset["test"],
     tokenizer=student_tokenizer,
     args=training_arguments,
-    max_seq_length=config["tokenizer"]["max_length"],
-    dataset_text_field="text",
 )
 
 # Add the teacher model to the trainer
--- quick_eval.py
+++ quick_eval.py
@@ -1,8 +1,12 @@
 import os
 import logging
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import random
+import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 from dotenv import load_dotenv
 from datasets import load_dataset
+import evaluate
 
 
 load_dotenv()
@@ -10,59 +14,87 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-model_name = "meta-llama/Llama-3.2-1B-Instruct"
-model_dir = f"./models/{model_name.replace('/', '_')}"
+# model_name = "meta-llama/Llama-3.2-3B-Instruct"
+# tokenizer = AutoTokenizer.from_pretrained(model_name, token=os.getenv("HF_TOKEN"))
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_name, device_map="auto", token=os.getenv("HF_TOKEN")
+# )
+
+model_path = "./results/checkpoint-504"
+config = AutoConfig.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+seed_value = 42  # You can choose any number
+random.seed(seed_value)
+np.random.seed(seed_value)
+torch.manual_seed(seed_value)
+torch.cuda.manual_seed_all(seed_value)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 try:
-    if not os.path.exists(model_dir):
-        logger.info("Model not found locally. Downloading from HuggingFace...")
-        tokenizer = AutoTokenizer.from_pretrained(
-            model_name, token=os.getenv("HF_TOKEN")
-        )
-        model = AutoModelForCausalLM.from_pretrained(
-            model_name, device_map="auto", token=os.getenv("HF_TOKEN")
-        )
-        os.makedirs(model_dir, exist_ok=True)
-        model.save_pretrained(model_dir)
-        tokenizer.save_pretrained(model_dir)
-    else:
-        logger.info("Loading model from local directory.")
-        tokenizer = AutoTokenizer.from_pretrained(model_dir)
-        model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto")
-except Exception as e:
-    logger.error("Error loading or downloading the model: %s", e)
-    raise
-
-try:
-    dataset = load_dataset("nicher92/medqa-swe")
+    dataset = load_dataset("qiaojin/PubMedQA", "pqa_artificial")
 except Exception as e:
     logger.error("Error loading dataset: %s", e)
     raise
 
+rouge = evaluate.load("rouge")
+bertscore = evaluate.load("bertscore")
+
+predictions = []
+references = []
+
 try:
-    tot = 0 # only count one-letter responses
-    correct = 0
-    for i in range(20):
+    for i in range(2):
+        if i == 0:
+            continue
         sample = dataset["train"][i]
         prompt = (
-            "You are a knowledgeable medical expert. Answer the following multiple-choice question by replying with only the letter corresponding to the correct option and nothing else. Your answer must consist of only one letter.\n\n"
-            f"Question: {sample['question']}\n\n"
-            f"Options: \n{sample['options']}\n\n"
-            f"Answer: "
+            f"You are a biomedical expert. Answer the following biomedical question. Provide a concise, evidence-backed response.\n\n"
+            f"Question: {sample['question']}"
         )
         inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-        outputs = model.generate(**inputs, max_new_tokens=100)
-        
+        outputs = model.generate(
+            **inputs,
+            max_new_tokens=512,
+            do_sample=False,  
+            num_beams=1,  
+            temperature=1.0,
+            top_p=1.0,  
+        )
+
         # skip the prompt tokens
-        prompt_tokens = inputs['input_ids'].shape[-1]
-        generated_response = tokenizer.decode(outputs[0][prompt_tokens:], skip_special_tokens=True)
-        
-        logger.info(f"Model answered: {generated_response}, expected: {sample['answer']}\n")
-        if len(generated_response.strip()) == 1:
-            tot += 1
-            if generated_response.strip() == sample['answer']:
-                correct += 1
-    logger.info(f"Accuracy: {correct/tot} ({correct}/{tot})")
+        prompt_tokens = inputs["input_ids"].shape[-1]
+        generated_response = tokenizer.decode(
+            outputs[0][prompt_tokens:], skip_special_tokens=True
+        )
+
+        logger.info(
+            f"\n\nPROMPT: {prompt}\n\n"
+            f"MODEL'S ANSWER: {generated_response}\n\n"
+            f"EXPECTED ANSWER: {sample['long_answer']}\n\n"
+            f"{'-' * 50}"
+        )
+
+        predictions.append(generated_response)
+        references.append(sample["long_answer"])
+
+    # rouge_results = rouge.compute(predictions=predictions, references=references)
+    # bertscore_results = bertscore.compute(
+    #     predictions=predictions, references=references, lang="en"
+    # )
+
+    # logger.info("Evaluation results:\n\n")
+    # logger.info(f"\n\nROUGE evaluation results: {rouge_results}")
+    # logger.info(f"\n\nBERTScore evaluation results: {bertscore_results}")
+
+    #     logger.info(f"Model answered: {generated_response}, expected: {sample['answer']}\n")
+    #     if len(generated_response.strip()) == 1:
+    #         tot += 1
+    #         if generated_response.strip() == sample['answer']:
+    #             correct += 1
+    # logger.info(f"Accuracy: {correct/tot} ({correct}/{tot})")
 
 except Exception as e:
     logger.error("Error during generation: %s", e)
