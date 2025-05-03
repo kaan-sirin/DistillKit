03428fdb04ac3af9565c4fa0515e2d64fd214133
kaan <kaansirin@yahoo.com>
2025-05-03 21:00:07 +0200

    run script updated to use 2 gpus

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=EUHPC_D17_084 # Update this with your from saldo -b
 #SBATCH --partition=boost_usr_prod # This can stay
 #SBATCH --qos=boost_qos_dbg # For production write "normal" here
-#SBATCH --gres=gpu:1 # GPUs PER NODE
+#SBATCH --gres=gpu:2 # GPUs PER NODE
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=0:15:00
+#SBATCH --cpus-per-task=16
+#SBATCH --time=1:00:00
 #SBATCH --mail-type=ALL # Sends you an email when it starts/stop
 #SBATCH --mail-user=kaansirin@yahoo.com
 
@@ -17,7 +17,7 @@ source .venv/bin/activate
 # Some environment variables to set up cache directories
 export TORCH_HOME=$WORK/torch-cache
 export HF_HOME=$WORK/hf-cache
-export GPUS_PER_NODE=1
+export GPUS_PER_NODE=2
 
 export HF_HUB_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
@@ -27,4 +27,4 @@ export WANDB_MODE=offline
 # want to retain direct control of parallelism options.
 export TOKENIZERS_PARALLELISM=false
 
-accelerate launch --num_processes $GPUS_PER_NODE distillation_on_cluster.py
+accelerate launch --num_processes $GPUS_PER_NODE distill_logits_final.py
