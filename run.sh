0617ef6b2a4491befa6e1e3e35d1db81c5ea21a1
kaan <kaansirin@yahoo.com>
2025-05-06 00:39:31 +0200

    updated sbatch script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=EUHPC_D17_084 # Update this with your from saldo -b
 #SBATCH --partition=boost_usr_prod # This can stay
-#SBATCH --qos=boost_qos_dbg # For production write "normal" here
-#SBATCH --gres=gpu:2 # GPUs PER NODE
+#SBATCH --qos=normal
+#SBATCH --gres=gpu:4 # GPUs PER NODE
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=ALL # Sends you an email when it starts/stop
 #SBATCH --mail-user=kaansirin@yahoo.com
 
@@ -17,7 +17,7 @@ source .venv/bin/activate
 # Some environment variables to set up cache directories
 export TORCH_HOME=$WORK/torch-cache
 export HF_HOME=$WORK/hf-cache
-export GPUS_PER_NODE=2
+export GPUS_PER_NODE=4
 
 export HF_HUB_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
@@ -27,4 +27,4 @@ export WANDB_MODE=offline
 # want to retain direct control of parallelism options.
 export TOKENIZERS_PARALLELISM=false
 
-accelerate launch --num_processes $GPUS_PER_NODE distill_logits_final.py
+accelerate launch --num_processes 1 distill_logits_final.py
