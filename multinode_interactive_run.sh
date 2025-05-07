05a0e9bebf133145402ba6aa11cb55c77362685f
kaan <kaansirin@yahoo.com>
2025-05-07 12:30:13 +0200

    new attempt multinode

--- multinode_interactive_run.sh
+++ multinode_interactive_run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # SLURM: 2 nodes, 4 GPUs each, 1 task per node
 srun -N 2 -n 2 \
-     --gpus-per-node=4 \
-     --cpus-per-task=32 \
-     -t 00:30:00 \
-     -A EUHPC_D17_084 \
-     -p boost_usr_prod \
-     --qos boost_qos_dbg \
-     bash -c '
+    --gpus-per-node=4 \
+    --cpus-per-task=32 \
+    -t 00:30:00 \
+    -A EUHPC_D17_084 \
+    -p boost_usr_prod \
+    --qos boost_qos_dbg \
+    bash -c '
     module load profile/deeplrn
     module load python cuda nccl cudnn
     source .venv/bin/activate
@@ -18,16 +18,14 @@ srun -N 2 -n 2 \
     
 
     # ---- distributed init ----
-    MASTER_NODE=$(scontrol show hostnames "$SLURM_NODELIST" | head -n 1)
-    export MASTER_ADDR=$MASTER_NODE
+    export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n1)
     export MASTER_PORT=29500
-    export WORLD_SIZE=$SLURM_NTASKS
-    export RANK=$SLURM_PROCID
-    export LOCAL_RANK=0           # one process per node
-
+     
     accelerate launch \
         --num_processes 1 \
         --num_machines 2 \
-        --machine_rank $RANK \
+        --machine_rank $SLURM_NODEID \
+        --main_process_ip $MASTER_ADDR \
+        --main_process_port $MASTER_PORT \
         distill_logits_final.py
 '
