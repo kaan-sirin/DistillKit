f593e53cdefc63646df23399c1310e708e4affec
kaan <kaansirin@yahoo.com>
2025-05-17 14:53:14 +0200

    sft script and config

--- sft_trainer.py
+++ sft_trainer.py
@@ -2,106 +2,122 @@ import os
 import torch
 from datasets import load_dataset
 from trl import SFTTrainer
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, EarlyStoppingCallback
 from accelerate import Accelerator
 import wandb
 from dotenv import load_dotenv
-from distillation_utils import load_config, medqa_format
+from distillation_utils import gsm8k_format, load_config, magpie_format, tokenize_function
 import time
-
-
-def tokenize_function(examples, tokenizer, config):
-    return tokenizer(
-        examples["text"],
-        truncation=True,
-        max_length=config["tokenizer"]["max_length"],
-        padding="max_length",
-    )
 
 
 def main():
     load_dotenv()
     HF_TOKEN = os.getenv("HF_TOKEN")
+    config = load_config('config_sft.yaml')
+    dataset_name = config["dataset"]["name"]
+    num_samples = config["dataset"].get("num_samples", None)
+    test_size = config["dataset"].get("test_size", 0.1)
+    dataset_config = load_config(f"datasets.yaml").get(dataset_name, None)
+    if dataset_config is None:
+        raise ValueError(f"Dataset config not found for {dataset_name}")
 
-    config = load_config()
-    run_name = f"sft_{config['dataset']['name'].split('/')[-1].replace('-', '_')}_{config['dataset']['num_samples']}samples_{config['training']['num_train_epochs']}epochs_{time.strftime('%m-%d_%H-%M').replace('-', '_')}"
-    # Output directory
+    group_name = f"sft_{dataset_name.split('/')[-1].replace('-', '_')}_"
+    if num_samples:
+        group_name += f"{num_samples}s_"
+    group_name += f"{config['training']['num_train_epochs']}e_{time.strftime('%m-%d_%H-%M').replace('-', '_')}"
+
     output_base = config["training"]["output_dir"]
-    output_dir = os.path.join(
-        output_base,
-        f"sft_{config['dataset']['name'].split('/')[-1].replace('-', '_')}_{config['dataset']['num_samples']}_{config['training']['num_train_epochs']}_{time.strftime('%m-%d_%H-%M').replace('-', '_')}",
-    )
-    os.makedirs(output_dir, exist_ok=True)
+    output_dir = os.path.join(output_base, group_name)
 
     # Set up environment
-    os.environ["WANDB_PROJECT"] = config["project_name"]
+    os.environ["WANDB_PROJECT"] = config["project_name"] + "-sft"
     accelerator = Accelerator()
-    device = accelerator.device
-
-    # Load and preprocess dataset
-    dataset = (
-        load_dataset(
-            config["dataset"]["name"],
-            config["dataset"]["subset"],
-            split=config["dataset"]["split"],
-        )
-        if config["dataset"].get("subset")
-        else load_dataset(config["dataset"]["name"], split=config["dataset"]["split"])
-    )
-
-    # First take a subset of the dataset if specified, then shuffle
-    if "num_samples" in config["dataset"]:
-        dataset = dataset.select(range(config["dataset"]["num_samples"]))
-    dataset = dataset.shuffle(seed=config["dataset"]["seed"])
+    run_name = f"process_{accelerator.process_index}_{time.strftime('%m_%d_%H_%M')}"
 
     # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(
-        config["models"]["student"], token=HF_TOKEN
+        config["model"], token=HF_TOKEN
     )
 
     if tokenizer.pad_token is None:
         tokenizer.pad_token = "<|finetune_right_pad_id|>"
 
+    # Load and preprocess dataset
+    dataset = (
+        load_dataset(
+            dataset_name,
+            dataset_config.get("subset"),
+            split=dataset_config.get("split"),
+        )
+        if dataset_config.get("subset")
+        else load_dataset(dataset_name, split=dataset_config.get("split"))
+    )
+
+    # First take a subset of the dataset if specified, then shuffle
+    if num_samples:
+        dataset = dataset.select(range(num_samples))
+    dataset = dataset.shuffle(seed=42)
+
     # Preprocess and tokenize the dataset
     print("Preprocessing and tokenizing dataset...")
     original_columns = dataset.column_names
-    dataset = dataset.map(medqa_format, remove_columns=original_columns)
+    if dataset_name== "nicher92/magpie_llama70b_260k_filtered_swedish":
+        dataset = dataset.filter(lambda x: x["task_category"] != "Math")
+        dataset = dataset.filter(lambda x: "Math" not in x["other_task_category"])
+        dataset = dataset.map(magpie_format, remove_columns=original_columns)
+    elif dataset_name == "openai/gsm8k":
+        dataset = dataset.map(gsm8k_format, remove_columns=original_columns)
+    else:
+        raise ValueError(
+            f"Make sure to specify a format function for {config['dataset']['name']} in {__file__}"
+        )
 
     tokenized_dataset = dataset.map(
-        lambda examples: tokenize_function(examples, tokenizer, config),
+        lambda examples: tokenize_function(
+            examples, tokenizer, config["tokenizer"]["max_length"]
+        ),
         batched=True,
         num_proc=8,
         remove_columns=["text"],
     )
 
-    tokenized_dataset = tokenized_dataset.train_test_split(test_size=0.1)
+    tokenized_dataset = tokenized_dataset.train_test_split(test_size=test_size)
 
     print("Dataset preparation complete. Loading model...")
-
-    # Load model with configurable flash attention
     model_kwargs = {"torch_dtype": torch.bfloat16}
-    if config["model_config"]["use_flash_attention"]:
-        model_kwargs["attn_implementation"] = "flash_attention_2"
-
     model = AutoModelForCausalLM.from_pretrained(
-        config["models"]["student"], **model_kwargs
+        config["model"], **model_kwargs
     )
 
     # Training arguments
-    training_arguments = TrainingArguments(
-        **config["training"],
-        report_to=["wandb"],
-    )
+    
+    training_args_dict = config["training"].copy()
+    training_args_dict["output_dir"] = output_dir
+    training_args_dict["report_to"] = ["wandb"]
+    early_stopping_patience = training_args_dict.pop("early_stopping_patience", 3)
+    training_arguments = TrainingArguments(**training_args_dict)
+
+    
+    callbacks = []
+    if training_arguments.load_best_model_at_end:
+        callbacks.append(
+            EarlyStoppingCallback(
+                early_stopping_patience=early_stopping_patience
+            )
+        )
 
     # Initialize wandb
-    wandb.init(
-        project=config["project_name"],
-        name=run_name,
-        config={
-            "model": config["models"]["student"],
+    if accelerator.is_main_process:
+        wandb.init(
+            project=config["project_name"],
+            name=run_name,
+            group=group_name,
+            config={
+            "model": config["model"],
             "num_samples": config["dataset"]["num_samples"],
             "num_epochs": config["training"]["num_train_epochs"],
         },
+        reinit=False,
     )
 
     # Create the SFT Trainer
@@ -111,6 +127,7 @@ def main():
         eval_dataset=tokenized_dataset["test"],
         tokenizer=tokenizer,
         args=training_arguments,
+        callbacks=callbacks,
     )
 
     # Prepare for distributed training
@@ -120,6 +137,7 @@ def main():
     trainer.train(resume_from_checkpoint=config["training"]["resume_from_checkpoint"])
 
     # Save the final model
+    os.makedirs(output_dir, exist_ok=True)
     trainer.save_model(output_dir)
 
 
