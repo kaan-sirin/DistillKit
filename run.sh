87bfb1c1188f77db82a4afc85f8271507a61f284
kaan <kaansirin@yahoo.com>
2025-05-17 21:15:01 +0200

    new run script

--- run.sh
+++ run.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-#SBATCH --account=EUHPC_D17_084 # Update this with your from saldo -b
-#SBATCH --partition=boost_usr_prod # This can stay
+#SBATCH --account=EUHPC_D17_084
+#SBATCH --partition=boost_usr_prod
 #SBATCH --qos=normal
-#SBATCH --gres=gpu:4 # GPUs PER NODE
-#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=1 
 #SBATCH --cpus-per-task=32
-#SBATCH --time=12:00:00
-#SBATCH --mail-type=ALL # Sends you an email when it starts/stop
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=kaansirin@yahoo.com
 
 module load profile/deeplrn
 module load python cuda nccl cudnn 
-
 source .venv/bin/activate
 
 export TORCH_HOME=$WORK/torch-cache
 export HF_HOME=$WORK/hf-cache
-
+export GPUS_PER_NODE=4
 export HF_HUB_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export WANDB_MODE=offline
-
 export TOKENIZERS_PARALLELISM=false
+export CUDA_VISIBLE_DEVICES=3
 
-accelerate launch --num_processes 1 distillation_new_attempt.py
+accelerate launch --num_processes $GPUS_PER_NODE distillation_new_attempt.py
