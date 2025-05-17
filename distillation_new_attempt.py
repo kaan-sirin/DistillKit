091da070a48d7e9d50f0c259dad75838ad2b85c0
kaan <kaansirin@yahoo.com>
2025-05-17 21:12:08 +0200

    small test run on cluster (loading 8B w/o 4-bit quantization)

--- distillation_new_attempt.py
+++ distillation_new_attempt.py
@@ -3,7 +3,12 @@ import torch
 import torch.nn.functional as F
 from datasets import load_dataset
 from trl import SFTTrainer
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, EarlyStoppingCallback
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    TrainingArguments,
+    EarlyStoppingCallback,
+)
 from accelerate import Accelerator
 import wandb
 from dotenv import load_dotenv
@@ -17,14 +22,15 @@ from distillation_utils import (
     magpie_format,
 )
 import time
-from transformers import BitsAndBytesConfig
+# from transformers import BitsAndBytesConfig
 
-bnb_cfg = BitsAndBytesConfig(
-    load_in_4bit=True,  # 4‑bit weights
-    bnb_4bit_quant_type="nf4",  # best accuracy
-    bnb_4bit_compute_dtype=torch.bfloat16,
-    bnb_4bit_use_double_quant=True,
-)
+# CHANGE 4
+# bnb_cfg = BitsAndBytesConfig(
+#     load_in_4bit=True,  # 4‑bit weights
+#     bnb_4bit_quant_type="nf4",  # best accuracy
+#     bnb_4bit_compute_dtype=torch.bfloat16,
+#     bnb_4bit_use_double_quant=True,
+# )
 
 
 # ------------------------------------------------------------
@@ -57,16 +63,16 @@ class LogitsTrainer(SFTTrainer):
     ):
         student_model = model.module if hasattr(model, "module") else model
         device = next(model.parameters()).device
-        inputs = {k: v.to(device) if hasattr(v, 'to') else v for k, v in inputs.items()}
-        
+        inputs = {k: v.to(device) if hasattr(v, "to") else v for k, v in inputs.items()}
+
         teacher_model = (
             self.teacher_model.module
             if hasattr(self.teacher_model, "module")
             else self.teacher_model
         )
-        self.teacher_model = self.teacher_model.to(device)
-        
-        
+        if next(teacher_model.parameters()).device != device: # CHANGE 1
+            self.teacher_model.to(device)
+
         student_outputs = student_model(**inputs)
         with torch.no_grad():
             teacher_outputs = teacher_model(**inputs)
@@ -93,9 +99,6 @@ class LogitsTrainer(SFTTrainer):
         teacher_logits_scaled = (
             teacher_logits / self.config["distillation"]["temperature"]
         )
-
-        student_logits_scaled = student_logits_scaled.to(torch.float16)
-        teacher_logits_scaled = teacher_logits_scaled.to(torch.float16)
 
         kld = None
         if self.use_reverse_kld:
@@ -147,14 +150,14 @@ class LogitsTrainer(SFTTrainer):
             self._accumulated_loss_kd = 0.0
             self._accumulated_original_loss = 0.0
             self._accumulation_step_count = 0
-        
+
         is_eval = any(k.startswith("eval_") for k in logs.keys())
 
         if is_eval and "eval_loss" not in logs and "loss" in logs:
             logs["eval_loss"] = logs["loss"]
 
         super().log(logs)
-    
+
     def prediction_step(self, model, inputs, prediction_loss_only, **_):
         with torch.no_grad(), self.compute_loss_context_manager():
             loss, _ = self.compute_loss(model, inputs, return_outputs=True)
@@ -165,12 +168,12 @@ def main():
     load_dotenv()
     HF_TOKEN = os.getenv("HF_TOKEN")
     config = load_config()
-    
+
     dataset_name = config["dataset"]["name"]
     num_samples = config["dataset"].get("num_samples", None)
     dataset_config = load_config("datasets.yaml")[dataset_name]
     test_size = config["dataset"].get("test_size", 0.1)
-    
+
     # Set up environment
     os.environ["WANDB_PROJECT"] = config["project_name"]
     accelerator = Accelerator(mixed_precision="bf16")
@@ -179,7 +182,7 @@ def main():
     if num_samples is not None:
         group_name += f"{num_samples}_samples_"
     group_name += f"{config['training']['num_train_epochs']}_epochs_{time.strftime('%m_%d_%H_%M')}"
-    
+
     run_name = f"process_{accelerator.process_index}_{time.strftime('%m_%d_%H_%M')}"
 
     # Output directory
@@ -199,9 +202,14 @@ def main():
     )
     if dataset_name == "Vezora/Tested-143k-Python-Alpaca":
         dataset = dataset.filter(lambda x: x["input"] == "")  # python alpaca quirk
-    elif dataset_name == "nicher92/magpie_llama70b_260k_filtered_swedish": # distill on non-math entries
-        dataset = dataset.filter(lambda x: (x["task_category"] != "Math" and "Math" not in x["other_task_category"]))
-
+    elif (
+        dataset_name == "nicher92/magpie_llama70b_260k_filtered_swedish"
+    ):  # distill on non-math entries
+        dataset = dataset.filter(
+            lambda x: (
+                x["task_category"] != "Math" and "Math" not in x["other_task_category"]
+            )
+        )
 
     # First take a subset of the dataset if specified, then shuffle
     # This is because I want to know which samples are used for training
@@ -232,19 +240,21 @@ def main():
         dataset = dataset.map(gsm8k_format, remove_columns=original_columns)
     elif dataset_name == "nicher92/magpie_llama70b_260k_filtered_swedish":
         dataset = dataset.map(magpie_format, remove_columns=original_columns)
-        
+
     else:
-        raise ValueError(f"Add the format function for {dataset_name} from distillation_utils.py")
+        raise ValueError(
+            f"Add the format function for {dataset_name} from distillation_utils.py"
+        )
     tokenized_dataset = dataset.map(
-        lambda examples: tokenize_function(examples, tokenizer, config["tokenizer"]["max_length"]),
+        lambda examples: tokenize_function(
+            examples, tokenizer, config["tokenizer"]["max_length"]
+        ),
         batched=True,
         num_proc=8,
         remove_columns=["text"],
     )
     if test_size is not None:
-        tokenized_dataset = tokenized_dataset.train_test_split(
-            test_size=test_size
-        )
+        tokenized_dataset = tokenized_dataset.train_test_split(test_size=test_size)
 
     print(
         f"Process {accelerator.process_index}: Dataset preparation complete. Loading models..."
@@ -258,7 +268,8 @@ def main():
 
     teacher_model = AutoModelForCausalLM.from_pretrained(
         config["models"]["teacher"],
-        quantization_config=bnb_cfg,
+        **model_kwargs,
+        # quantization_config=bnb_cfg # CHANGE 4
     )
     teacher_model.eval().requires_grad_(False)
 
@@ -266,24 +277,21 @@ def main():
     student_model = AutoModelForCausalLM.from_pretrained(
         config["models"]["student"],
         **model_kwargs,
-    ) #TODO: Configure better placement of model on GPU
-    student_model.gradient_checkpointing_enable()
+    )  # TODO: Configure better placement of model on GPU
 
+   
     training_args_dict = config["training"].copy()
     training_args_dict["output_dir"] = output_dir  # Ensure output_dir is set
     training_args_dict["report_to"] = ["wandb"]
     training_args_dict["optim"] = "paged_adamw_8bit"
     early_stopping_patience = training_args_dict.pop("early_stopping_patience", 3)
     training_arguments = TrainingArguments(**training_args_dict)
-    
+
     callbacks = []
     if training_arguments.load_best_model_at_end:
         callbacks.append(
-            EarlyStoppingCallback(
-                early_stopping_patience=early_stopping_patience
-            )
+            EarlyStoppingCallback(early_stopping_patience=early_stopping_patience)
         )
-
 
     if accelerator.is_main_process:
         wandb.init(
@@ -313,6 +321,7 @@ def main():
         eval_dataset=tokenized_dataset["test"],
         args=training_arguments,
         config=config,
+        callbacks=callbacks,
     )
 
     # Prepare for distributed training
